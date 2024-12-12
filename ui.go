68ab6aa60a4e285e4763fedc785d9634ab718564
Chris Erin <chris.erin@gmail.com>
2024-12-12 17:44:22 -0500

    feat(ui): view seq grid

--- ui.go
+++ ui.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type keymap struct {
@@ -30,11 +32,27 @@ func (k keymap) FullHelp() [][]key.Binding {
 	}
 }
 
+const BLANK = " "
+
+var zeroRune rune
+
+type line []rune
+
 type model struct {
 	keys  keymap
 	beats int
 	tempo int
 	help  help.Model
+	lines []line
+}
+
+func InitSeq(lineNumber int, beatNumber int) []line {
+	var lines = make([]line, 0, lineNumber)
+
+	for i := 0; i < lineNumber; i++ {
+		lines = append(lines, make([]rune, beatNumber))
+	}
+	return lines
 }
 
 func InitModel() model {
@@ -43,6 +61,7 @@ func InitModel() model {
 		beats: 32,
 		tempo: 240,
 		help:  help.New(),
+		lines: InitSeq(8, 32),
 	}
 }
 
@@ -69,7 +88,42 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	var buf strings.Builder
+	buf.WriteString("   Seq - A sequencer for your cli\n")
+	buf.WriteString("  ┌────────────────────────────────────\n")
+	for i, line := range m.lines {
+		buf.WriteString(Line(i, line, m))
+	}
 	buf.WriteString(m.help.View(m.keys))
+	buf.WriteString("\n")
+	return buf.String()
+}
+
+var altSeqColor = lipgloss.NewStyle().Background(lipgloss.Color("#222222"))
+
+func Line(lineNumber int, line line, m model) string {
+	var buf strings.Builder
+	buf.WriteString(fmt.Sprintf("%d │", lineNumber))
+
+	for i := 0; i < m.beats; i++ {
+
+		var char string
+		if line[i] == zeroRune {
+			char = BLANK
+		} else {
+			char = string(line[i])
+		}
+		// if m.cursorPos.lineNumber == lineNumber && m.cursorPos.beat == i {
+		// 	m.cursor.SetChar(char)
+		// 	char = m.cursor.View()
+		// }
+
+		if i%8 > 3 {
+			buf.WriteString(altSeqColor.Render(char))
+		} else {
+			buf.WriteString(char)
+		}
+	}
+
 	buf.WriteString("\n")
 	return buf.String()
 }
