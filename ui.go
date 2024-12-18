641e8281920b2cb9f9351b2e37a159f9ac5eb86a
Chris Erin <chris.erin@gmail.com>
2024-12-18 11:48:15 -0500

    feat(ui): Add Tempo and Subdivisions

--- ui.go
+++ ui.go
@@ -18,17 +18,20 @@ import (
 )
 
 type keymap struct {
-	Quit          key.Binding
-	Help          key.Binding
-	CursorUp      key.Binding
-	CursorDown    key.Binding
-	CursorLeft    key.Binding
-	CursorRight   key.Binding
-	TriggerAdd    key.Binding
-	TriggerRemove key.Binding
-	PlayStop      key.Binding
-	ClearLine     key.Binding
-	ClearSeq      key.Binding
+	Quit             key.Binding
+	Help             key.Binding
+	CursorUp         key.Binding
+	CursorDown       key.Binding
+	CursorLeft       key.Binding
+	CursorRight      key.Binding
+	TriggerAdd       key.Binding
+	TriggerRemove    key.Binding
+	PlayStop         key.Binding
+	ClearLine        key.Binding
+	ClearSeq         key.Binding
+	TempoInputSwitch key.Binding
+	TempoIncrease    key.Binding
+	TempoDecrease    key.Binding
 }
 
 func Key(keyboardKey string, help string) key.Binding {
@@ -36,17 +39,20 @@ func Key(keyboardKey string, help string) key.Binding {
 }
 
 var keys = keymap{
-	Quit:          Key("q", "Quit"),
-	Help:          Key("?", "Expand Help"),
-	CursorUp:      Key("k", "Up"),
-	CursorDown:    Key("j", "Down"),
-	CursorLeft:    Key("h", "Left"),
-	CursorRight:   Key("l", "Right"),
-	TriggerAdd:    Key("f", "Add Trigger"),
-	TriggerRemove: Key("d", "Remove Trigger"),
-	ClearLine:     Key("c", "Clear Line"),
-	ClearSeq:      Key("C", "Clear Seq"),
-	PlayStop:      Key(" ", "Play/Stop"),
+	Quit:             Key("q", "Quit"),
+	Help:             Key("?", "Expand Help"),
+	CursorUp:         Key("k", "Up"),
+	CursorDown:       Key("j", "Down"),
+	CursorLeft:       Key("h", "Left"),
+	CursorRight:      Key("l", "Right"),
+	TriggerAdd:       Key("f", "Add Trigger"),
+	TriggerRemove:    Key("d", "Remove Trigger"),
+	ClearLine:        Key("c", "Clear Line"),
+	ClearSeq:         Key("C", "Clear Seq"),
+	PlayStop:         Key(" ", "Play/Stop"),
+	TempoInputSwitch: Key("T", "Select Tempo Indicator"),
+	TempoIncrease:    Key("+", "Tempo Increase"),
+	TempoDecrease:    Key("-", "Tempo Decrease"),
 }
 
 func (k keymap) ShortHelp() []key.Binding {
@@ -77,18 +83,20 @@ type CursorPosition struct {
 }
 
 type model struct {
-	keys        keymap
-	beats       int
-	tempo       int
-	help        help.Model
-	lines       []line
-	cursorPos   CursorPosition
-	cursor      cursor.Model
-	outport     drivers.Out
-	playing     bool
-	playTime    time.Time
-	totalBeats  int
-	currentBeat int
+	keys                    keymap
+	beats                   int
+	tempo                   int
+	subdivisions            int
+	help                    help.Model
+	lines                   []line
+	cursorPos               CursorPosition
+	cursor                  cursor.Model
+	outport                 drivers.Out
+	playing                 bool
+	playTime                time.Time
+	totalBeats              int
+	currentBeat             int
+	tempoSelectionIndicator uint8
 }
 
 type beatMsg struct{}
@@ -152,14 +160,15 @@ func InitModel() model {
 	}
 
 	return model{
-		keys:      keys,
-		beats:     32,
-		tempo:     240,
-		help:      help.New(),
-		lines:     InitSeq(8, 32),
-		cursorPos: CursorPosition{lineNumber: 0, beat: 0},
-		cursor:    newCursor,
-		outport:   outport,
+		keys:         keys,
+		beats:        32,
+		tempo:        120,
+		subdivisions: 2,
+		help:         help.New(),
+		lines:        InitSeq(8, 32),
+		cursorPos:    CursorPosition{lineNumber: 0, beat: 0},
+		cursor:       newCursor,
+		outport:      outport,
 	}
 }
 
@@ -231,6 +240,28 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				}
 				return m, tea.Batch(PlayBeat(m.lines, m.currentBeat, sendFn), BeatTick(m.playTime, m.totalBeats, m.tempo))
 			}
+		case Is(msg, m.keys.TempoInputSwitch):
+			m.tempoSelectionIndicator = (m.tempoSelectionIndicator + 1) % 3
+		case Is(msg, m.keys.TempoIncrease):
+			if m.tempoSelectionIndicator == 1 {
+				if m.tempo < 300 {
+					m.tempo++
+				}
+			} else if m.tempoSelectionIndicator == 2 {
+				if m.subdivisions < 8 {
+					m.subdivisions++
+				}
+			}
+		case Is(msg, m.keys.TempoDecrease):
+			if m.tempoSelectionIndicator == 1 {
+				if m.tempo > 30 {
+					m.tempo--
+				}
+			} else if m.tempoSelectionIndicator == 2 {
+				if m.subdivisions > 1 {
+					m.subdivisions--
+				}
+			}
 		}
 		if msg.String() >= "1" && msg.String() <= "9" {
 			beatInterval, _ := strconv.Atoi(msg.String())
@@ -272,7 +303,45 @@ func fill(beatline line, start int, every int) line {
 	return beatline
 }
 
+var heartColor = lipgloss.NewStyle().Foreground(lipgloss.Color("#ed3902"))
+var selectedColor = lipgloss.NewStyle().Background(lipgloss.Color("#5cdffb")).Foreground(lipgloss.Color("#000000"))
+var numberColor = lipgloss.NewStyle().Foreground(lipgloss.Color("#fcbd15"))
+
+func (m model) TempoView() string {
+	var buf strings.Builder
+	var tempo, division string
+	switch m.tempoSelectionIndicator {
+	case 0:
+		tempo = numberColor.Render(strconv.Itoa(m.tempo))
+		division = numberColor.Render(strconv.Itoa(m.subdivisions))
+	case 1:
+		tempo = selectedColor.Render(strconv.Itoa(m.tempo))
+		division = numberColor.Render(strconv.Itoa(m.subdivisions))
+	case 2:
+		tempo = numberColor.Render(strconv.Itoa(m.tempo))
+		division = selectedColor.Render(strconv.Itoa(m.subdivisions))
+	}
+	heart := heartColor.Render("♡")
+	buf.WriteString(heartColor.Render("             ") + "\n")
+	buf.WriteString(heartColor.Render("   ♡♡♡☆ ☆♡♡♡ ") + "\n")
+	buf.WriteString(heartColor.Render("  ♡    ◊    ♡") + "\n")
+	buf.WriteString(heartColor.Render("  ♡  TEMPO  ♡") + "\n")
+	buf.WriteString(fmt.Sprintf("  %s   %s   %s\n", heart, tempo, heart))
+	buf.WriteString(heartColor.Render("   ♡ BEATS ♡") + "\n")
+	buf.WriteString(fmt.Sprintf("    %s  %s  %s  \n", heart, division, heart))
+	buf.WriteString(heartColor.Render("     ♡   ♡   ") + "\n")
+	buf.WriteString(heartColor.Render("      ♡ ♡    ") + "\n")
+	buf.WriteString(heartColor.Render("       †     ") + "\n")
+	return buf.String()
+}
+
 func (m model) View() string {
+	var buf strings.Builder
+	buf.WriteString(lipgloss.JoinHorizontal(0, m.TempoView(), "  ", m.ViewTriggerSeq()))
+	return buf.String()
+}
+
+func (m model) ViewTriggerSeq() string {
 	var buf strings.Builder
 	buf.WriteString("   Seq - A sequencer for your cli\n")
 	buf.WriteString("  ┌────────────────────────────────────\n")
