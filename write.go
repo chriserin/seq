87785249489036f2f0e6bb87aef83c180e6c00f4
Chris Erin <chris.erin@gmail.com>
2025-01-17 11:27:53 -0500

    feat(file): Timestamped file name in seq dir

--- write.go
+++ write.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/BurntSushi/toml"
 )
@@ -28,10 +33,6 @@ func TransformMetaOverlays(metaOverlays map[overlayKey]metaOverlay) WriteMetaOve
 }
 
 type WriteOverlay map[string]note
-type WriteMetaOverlay struct {
-	pressUp   bool
-	pressDown bool
-}
 
 func TransformOverlay(grid overlay) WriteOverlay {
 	transformedOverlay := make(WriteOverlay)
@@ -83,6 +84,9 @@ func UnMarshalOverlayKey(k string) overlayKey {
 	parts := strings.Split(k, "-")
 	keyParts := strings.Split(parts[1], "/")
 	num, err := strconv.Atoi(keyParts[0])
+	if err != nil {
+		panic("Could not convert keyParts to str")
+	}
 	denom, err := strconv.Atoi(keyParts[1])
 	if err != nil {
 		panic("Could not unmarshal overlaykey")
@@ -94,6 +98,9 @@ func UnMarshalGridKey(k string) gridKey {
 	// Grid-00-00
 	parts := strings.Split(k, "-")
 	line, err := strconv.Atoi(parts[1])
+	if err != nil {
+		panic("Unable to deserialize gridKey")
+	}
 	beat, err := strconv.Atoi(parts[2])
 	if err != nil {
 		panic("Unable to deserialize gridKey")
@@ -103,7 +110,12 @@ func UnMarshalGridKey(k string) gridKey {
 
 func Read() (Definition, bool) {
 	all := &AllWrite{}
-	toml.DecodeFile("saveme.toml", all)
+	files, _ := getSeqFileNames()
+	if len(files) > 0 {
+		_, _ = toml.DecodeFile(files[len(files)-1], all)
+	} else {
+		return Definition{}, false
+	}
 	return Definition{
 		overlays:     (*all).UntransformOverlays(),
 		lines:        (*all).LineDefinitions,
@@ -116,7 +128,11 @@ func Read() (Definition, bool) {
 }
 
 func Write(definition Definition) {
-	f, err := os.Create("saveme.toml")
+
+	fileName := newFilename()
+	dirname, _ := CreateSeqDir()
+	fullFilePath := filepath.Join(dirname, fileName)
+	f, err := os.Create(fullFilePath)
 	if err != nil {
 		panic("saveme file not saved")
 	}
@@ -134,4 +150,45 @@ func Write(definition Definition) {
 	if err != nil {
 		panic("could not encode " + err.Error())
 	}
+}
+
+var defaultSeqDir = ".seq"
+var extension = ".toml"
+
+func SeqDirName() string {
+	workingDir, _ := os.Getwd()
+	dirPath := filepath.Join(workingDir, defaultSeqDir)
+	return dirPath
+}
+
+func CreateSeqDir() (string, error) {
+	dirPath := SeqDirName()
+	err := os.MkdirAll(dirPath, 0755)
+	return dirPath, err
+}
+
+var PGEX_DATE_FORMAT = "20060102150405"
+
+func newFilename() string {
+	formattedNow := time.Now().Format(PGEX_DATE_FORMAT)
+	return fmt.Sprintf("%s_%s%s", formattedNow, "seq", extension)
+}
+
+func getSeqFileNames() ([]string, error) {
+	dirname := SeqDirName()
+	dirEntries, err := os.ReadDir(dirname)
+	if err != nil {
+		return []string{}, errors.New("_pgex dir does not exist, use the exec command to create a .pgex file in a _pgex dir")
+	}
+
+	seqFiles := make([]string, 0, len(dirEntries))
+	for _, d := range dirEntries {
+		pgexFile := regexp.MustCompile(`[0-9]{14}_seq\.toml`)
+		if pgexFile.Match([]byte(d.Name())) {
+			result := filepath.Join(dirname, d.Name())
+			seqFiles = append(seqFiles, result)
+		}
+	}
+
+	return seqFiles, nil
 }
