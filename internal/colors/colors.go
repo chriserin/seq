3305e92486384e7038ad5cd036d1a291575582cc
Chris Erin <chris.erin@gmail.com>
2025-04-15 17:26:46 -0400

    Refactor: Reorganized color file

--- internal/colors/colors.go
+++ internal/colors/colors.go
@@ -13,13 +13,10 @@ var NumberColor = lipgloss.Color("#fcbd15")
 var Black = lipgloss.Color("#000000")
 var White = lipgloss.Color("#ffffff")
 var Heart = lipgloss.Color("#ed3902")
-
-var ActiveRatchetColor lipgloss.Color = "#abfaa9"
-var MutedRatchetColor lipgloss.Color = "#f34213"
-
-var currentPlayingColor lipgloss.Color = "#abfaa9"
-var activePlayingColor lipgloss.Color = "#f34213"
-
+var ActiveRatchetColor = lipgloss.Color("#abfaa9")
+var MutedRatchetColor = lipgloss.Color("#f34213")
+var currentPlayingColor = lipgloss.Color("#abfaa9")
+var activePlayingColor = lipgloss.Color("#f34213")
 var ArrangementHeaderColor = lipgloss.Color("FAFAFA")
 var ArrangementTitleColor = lipgloss.Color("FAFAFA")
 var ArrangementGroupColor = lipgloss.Color("#F25D94")
@@ -28,16 +25,13 @@ var ArrangementSelectedLineColor = lipgloss.Color("#3b4261")
 
 var ActiveStyle = lipgloss.NewStyle().Foreground(ActiveRatchetColor)
 var MutedStyle = lipgloss.NewStyle().Foreground(MutedRatchetColor)
-
 var HeartStyle = lipgloss.NewStyle().Foreground(Heart)
 var SelectedStyle = lipgloss.NewStyle().Background(SelectedAttributeColor).Foreground(Black)
 var NumberStyle = lipgloss.NewStyle().Foreground(NumberColor)
+var AccentModeStyle = lipgloss.NewStyle().Background(Heart).Foreground(Black)
+var BlackKeyStyle = lipgloss.NewStyle().Background(Black).Foreground(White)
+var WhiteKeyStyle = lipgloss.NewStyle().Background(White).Foreground(Black)
 
 var CurrentlyPlayingSymbol = lipgloss.NewStyle().Foreground(currentPlayingColor).Render(" \u25CF ")
 var OverlayCurrentlyPlayingSymbol = lipgloss.NewStyle().Background(SeqOverlayColor).Foreground(currentPlayingColor).Render(" \u25CF ")
 var ActiveSymbol = lipgloss.NewStyle().Background(SeqOverlayColor).Foreground(activePlayingColor).Render(" \u25C9 ")
-
-var AccentModeStyle = lipgloss.NewStyle().Background(Heart).Foreground(Black)
-
-var BlackKeyStyle = lipgloss.NewStyle().Background(Black).Foreground(White)
-var WhiteKeyStyle = lipgloss.NewStyle().Background(White).Foreground(Black)
