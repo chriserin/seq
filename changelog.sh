45e0d68a3125ecbfb7f358e928d893a63ffc7e81
Chris Erin <chris.erin@gmail.com>
2025-07-16 16:05:16 +0000

    chore: Add release/changelog scripts

---
+++ changelog.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+startTag=$(git tag --list --sort=version:refname | tail -n 1)
+lastTag=$(git tag --list --sort=version:refname | tail -n 2 | head -n 1)
+#
+# startTag=HEAD
+# lastTag=cf40913
+
+if [ -z "$startTag" ]; then
+    echo "No tags found. Please create a tag first."
+    exit 1
+fi
+
+if [ -z "$lastTag" ]; then
+    echo "Only one tag found: $startTag. No changes to show."
+    exit 0
+fi
+
+date=$(git log -1 --format=%cd --date=short "$startTag")
+
+echo "## [${startTag}](github.com/chriserin/seq/compare/${startTag}...${lastTag}) ($date)"
+echo ""
+
+features=$(git log --oneline "${startTag}"..."${lastTag}" | awk ' $2 ~ /^feat/ {print}')
+
+if [ -n "$features" ]; then
+    echo "### Features"
+    echo ""
+    while IFS= read -r line; do
+        commit_hash=$(echo "$line" | awk '{print $1}')
+        commit_message=$(echo "$line" | cut -d' ' -f3-)
+        echo "* $commit_message [${commit_hash}](github.com/chriserin/seq/commit/${commit_hash}) "
+    done <<<"$features"
+else
+    echo "### Features"
+    echo ""
+    echo "No new features."
+    echo ""
+fi
+
+fixes=$(git log --oneline "${startTag}"..."${lastTag}" | awk ' $2 ~ /^fix/ {print}')
+
+if [ -n "$fixes" ]; then
+    echo ""
+    echo "### Fixes"
+    echo ""
+    while IFS= read -r line; do
+        commit_hash=$(echo "$line" | awk '{print $1}')
+        commit_message=$(echo "$line" | cut -d' ' -f3-)
+        echo "* $commit_message [${commit_hash}](github.com/chriserin/seq/commit/${commit_hash}) "
+    done <<<"$fixes"
+else
+    echo "### Fixes"
+    echo ""
+    echo "No bug fixes."
+    echo ""
+fi
---
+++ release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+tag="$1"
+
+git tag "$tag" -m "Release $tag"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to create tag $tag. Please check if the tag already exists."
+    exit 1
+fi
+
+echo "Tag $tag created successfully."
+
+git push && git push --tags
+
+if [ $? -ne 0 ]; then
+    echo "Failed to push changes and tags to the remote repository."
+    exit 1
+fi
+
+echo "Changes and tags pushed successfully."
+
+changeLogEntry=$(./changelog.sh)
+
+currentChangeLog=$(tail -n +2 CHANGELOG.md)
+
+{
+    echo "# CHANGELOG",
+    echo "",
+    echo "$changeLogEntry",
+    echo "",
+    echo "$currentChangeLog",
+} >CHANGELOG.md
+
+git add CHANGELOG.md
+git commit -m "chore: Update CHANGELOG.md for release $tag"
+git push
